3447cdafcca13144f5cd32ef9fb63ff5e1b10589
Craig Donavin <crdonavin@gmail.com>
2020-02-03 01:58:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,16 @@ let Calculator = {
  add: function(a,b) {
     return (a + b)
   }
+
+  subtract: function(a,b) {
+    return (a-b)
+  }
+
+  multiply: function(a,b) {
+      return (a*b)
+  }
+
+  divide: function(a,b) {
+    return (a/b)
+  }
 }
