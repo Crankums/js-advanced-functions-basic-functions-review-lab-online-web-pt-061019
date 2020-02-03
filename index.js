41645323707bd703da9bf191968adc534c7e896f
Craig Donavin <crdonavin@gmail.com>
2020-02-03 01:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ let wrapAdjective = function(flair = "*"){
 let Calculator = {
  add: function(a,b) {
     return (a + b)
-  }
+  },
 
   subtract: function(a,b) {
     return (a-b)
-  }
+  },
 
   multiply: function(a,b) {
       return (a*b)
-  }
+  },
 
   divide: function(a,b) {
     return (a/b)
